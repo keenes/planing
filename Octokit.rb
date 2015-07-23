5c18ede77bbab60badc7f17ca84b3c1b71ca25ab
keenes <keenesthan@yahoo.fr>
2015-07-23 18:54:05 +0200

    Create new file

---
+++ Octokit.rb
@@ -0,0 +1,7 @@
+# !!! DO NOT EVER USE HARD-CODED VALUES IN A REAL APP !!!
+# Instead, set and test environment variables, like below
+ACCESS_TOKEN = ENV['MY_PERSONAL_TOKEN']
+
+before do
+  @client ||= Octokit::Client.new(:access_token => ACCESS_TOKEN)
+end
