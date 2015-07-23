d934bc5864da297044324753411287219adeae92
keenes <keenesthan@yahoo.fr>
2015-07-23 18:55:30 +0200

    Update Octokit.rb

--- Octokit.rb
+++ Octokit.rb
@@ -3,5 +3,5 @@
 ACCESS_TOKEN = ENV['MY_PERSONAL_TOKEN']
 
 before do
-  @client ||= Octokit::Client.new(:access_token => ACCESS_TOKEN)
+  @client ||= Octokit::Client.new(:access_token => '96d9fb10f3bac630031f984dd0c7d7ba3ba19558')
 end
