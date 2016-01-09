2086be14e7f71a4f130b809bb5279bf692af3c55
keenes <keenes@users.noreply.github.com>
2016-01-09 21:50:38 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,17 +1,3 @@
-#!/usr/lib/ruby/gems/1.8/gems/ruby-lsapi-4.0
-planing.txt
+#!/user/bin/ Bash.exe
 
-mult inform sur github
 
-You can specify terminal blocks with  pre.terminal  elements. (It'd be nice if Markdown could do this more cleanly.)
-
-<pre class="terminal">
-$ curl foobar
-....
-</pre>
-
-donc <style>
-pre.terminal{color:#eeeeee;}</style>
-
- *  https://github.com/github
- *  https://github.com/github/developer.github.com
